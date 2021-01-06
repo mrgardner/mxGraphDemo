2473cb79b1b0bcd967b2a5b58aef141ab63bfe71
Matthew Gardner <c-matt.gardner@charter.com>
2021-01-06 13:27:01 -0500

    Add method to draw a new shape

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,32 +8,53 @@ import factory from 'mxgraph';
 })
 export class AppComponent implements AfterViewInit {
   title = 'mxGraphDemo';
+  private graph: any;
+  private mx: any;
+  private vertexId: number = 1;
 
   @ViewChild('graphContainer') graphContainer!: ElementRef;
 
-  ngAfterViewInit() {
-    const mx = factory({
+  constructor() {
+    this.mx = factory({
       mxBasePath: ''
     });
+  }
 
-    const newGraph = new mx.mxGraph(this.graphContainer.nativeElement);
+  ngAfterViewInit() {
+
+    this.graph = new this.mx.mxGraph(this.graphContainer.nativeElement);
     try {
-      const parent = newGraph.getDefaultParent();
-      newGraph.getModel().beginUpdate();
-      const vertex1 = newGraph.insertVertex(parent, '1', 'Vertex 1', 0, 0, 200, 80);
-      const vertex2 = newGraph.insertVertex(parent, '2', 'Vertex 2', 0, 0, 200, 80);
-      newGraph.insertEdge(parent, '', '', vertex1, vertex2);
+      const parent = this.graph.getDefaultParent();
+      this.graph.getModel().beginUpdate();
+      const vertex1 = this.graph.insertVertex(parent, this.vertexId, `Vertex ${this.vertexId}`, 0, 0, 200, 80);
+      this.vertexId = this.vertexId + 1;
+      const vertex2 = this.graph.insertVertex(parent, this.vertexId, `Vertex ${this.vertexId}`, 0, 0, 200, 80);
+      this.vertexId = this.vertexId + 1;
+      this.graph.insertEdge(parent, '', '', vertex1, vertex2);
     } finally {
-      newGraph.getModel().endUpdate();
-      new mx.mxHierarchicalLayout(newGraph).execute(newGraph.getDefaultParent());
+      this.graph.getModel().endUpdate();
+      new this.mx.mxHierarchicalLayout(this.graph).execute(this.graph.getDefaultParent());
     }
-    newGraph.addListener(mx.mxEvent.CLICK, (sender:any, evt: any) => {
+    this.graph.addListener(this.mx.mxEvent.CLICK, (sender:any, evt: any) => {
       const vertexID = evt.getProperty('cell');
+      console.log(vertexID);
       if(vertexID ) {
       console.log(`Vertex with id ${vertexID.id} was pressed`)
       } else {
       console.log(`Graph was clicked.`)
       }
     });
+  }
+
+  drawShape() {
+    try {
+      const parent = this.graph.getDefaultParent();
+      this.graph.getModel().beginUpdate();
+      const vertex1 = this.graph.insertVertex(parent, this.vertexId, `Vertex ${this.vertexId}`, 0, 0, 200, 80);
+      this.vertexId = this.vertexId + 1;
+    } finally {
+      this.graph.getModel().endUpdate();
+      new this.mx.mxHierarchicalLayout(this.graph).execute(this.graph.getDefaultParent());
+    }
   }
 }
